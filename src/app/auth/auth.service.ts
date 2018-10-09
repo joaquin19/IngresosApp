46b361e1f8e4802d07c03d2cd969809622e89e3f
Joaquin Alejandro Navedo Vazquez <e-jnavedo@neoris.cxnetworks.net>
2018-10-09 17:15:39 -0500

    base de datos terminada login y creacion de cuenta 2/2

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor( private angularFireAuth: AngularFireAuth ) { }
+  constructor( private angularFireAuth: AngularFireAuth,
+               private router: Router ) { }
 
   createUser( name: string, email: string, password: string) {
 
@@ -14,8 +16,24 @@ export class AuthService {
     .createUserWithEmailAndPassword(email, password)
     .then( resp => {
       console.log(resp);
+
+      // es la ruta de cuando se crea la contraseña ingresa al dashboard
+      this.router.navigate(['/']);
     })
     .catch( error => {
+      console.error(error);
+    });
+  }
+
+  loginUser( email: string, password: string) {
+    this.angularFireAuth.auth
+    .signInWithEmailAndPassword(email, password)
+    .then(resp => {
+      console.log(resp);
+
+      this.router.navigate(['/']);
+    })
+    .catch(error => {
       console.error(error);
     });
   }
